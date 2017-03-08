3bc30d5ee5e9f093f4b4b5748e8bb720d89cbde1
Xiao Ling <lingxiao@seas.upenn.edu>
2017-03-08 14:19:56 -0500

    hw3 chekcpoint

--- hw3/src/twitter.py
+++ hw3/src/twitter.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# @Use: python twitter.py --decode --data_dir /Users/lingxiao/Documents/research/dialogue-systems/hw3/data/tworkenized --train_dir /Users/lingxiao/Documents/research/dialogue-systems/hw3/checkpoints
+#
 # ==============================================================================
 
 """
